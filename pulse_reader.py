750a32a95290a385c01c89baa0fe29f2bd7cd07d
Brian B <bauma2bm@cmich.edu>
2015-12-09 03:20:43 +0000

    Initial Project upload

---
+++ pulse_reader.py
@@ -0,0 +1,29 @@
+from smbus import SMBus
+from time import sleep
+import RPi.GPIO as GPIO
+
+# compatibility for different versions of RasPi
+# rev 2 or 3 will use bus address 1
+# rev 1 will use bus address 0
+rev = GPIO.RPI_REVISION
+if rev == 2 or rev == 3:
+	b = SMBus(1)
+else:
+	b = SMBus(0)
+
+# test to see if this is always I2C-1
+address = 0x50
+
+# Pulse will return -1 in case of immediate IOError
+pulse = -1
+
+while True:
+	# handling IOError incase Grove decides to go on a walk
+	try:
+		pulse = b.read_byte(address)
+	except IOError:
+		print("there was an IO Error")
+	# print the pulse otherwise
+	print(pulse)
+	# wait a second
+	sleep(1)
